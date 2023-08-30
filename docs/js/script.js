4a7866e252020c1476bf46ca7a0af4a67f87996a
BastianMariangel <bastianmariangel@gmail.com>
2023-08-30 19:19:25 -0400

    mod1

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,8 +1,6 @@
 
-    // Fetch the contents of the navbar.html file
     fetch('navbar.html')
     .then(response => response.text())
     .then(navbarHTML => {
-    // Inject the navbar HTML into the designated element
     document.getElementById('navbar-placeholder').innerHTML = navbarHTML;
 });
